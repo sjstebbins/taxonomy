742703e415cf0554b4eed7724c03897c9169c084
spencer stebbins <sjfstebbins@gmail.com>
2014-11-03 14:11:57 -0600

    compelted taxonomy

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.canFly = true;
+  this.treasureChest = [];
+  this.job = 'find pollen';
 };
+
+ForagerBee.prototype = new Bee();
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+};
+
 
